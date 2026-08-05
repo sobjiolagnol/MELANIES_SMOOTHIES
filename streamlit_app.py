d5a1c31779eb1866c2a927012e66c7e74868ec74
Lagnol SOBJIO <lagnolsobjio@yahoo.fr>
2026-08-05 23:41:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,16 +62,16 @@ if ingredients_list:
                 f"Your Smoothie is ordered, {name_on_order.strip()}!",
                 icon="✅"
             )
-# ================================
-# AJOUTE LE CODE ICI
-# ================================
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
 
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 
 
 
