c9745bfbde060732ef4193b8ce9c842a6022f2d1
Lagnol SOBJIO <lagnolsobjio@yahoo.fr>
2026-08-05 13:50:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+#st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title('My Parents New Healthy Diner')
 
 st.write(
     """
